1665fedaeca742731d3e6a098306ef521ff33453
PenguinPepper <pfachielizabeth@gmail.com>
2025-07-29 11:29:32 +0200

    Use different connection key

--- backend/config/supabase.ts
+++ backend/config/supabase.ts
@@ -3,9 +3,9 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const supabaseUrl = process.env.SUPABASE_URL as string;
-const supabaseAnonKey = process.env.SUPABASE_ANON_KEY as string;
+const supabaseServiceKey = process.env.SUPABASE_SERVICE_ROLE_KEY as string;
 const supabase: SupabaseClient = createClient(supabaseUrl,
-  supabaseAnonKey, {
+  supabaseServiceKey, {
     auth: {
         persistSession: false,
         autoRefreshToken: true,
